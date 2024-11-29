e860bdc42e73778d2b9b4fb219593e3e466ea135
Denis Perov <denis.perov@gmail.com>
2024-11-29 19:55:53 +0400

    Add seamless Open Frames standard support

--- nodes/FrameBuilder/FrameBuilder.node.ts
+++ nodes/FrameBuilder/FrameBuilder.node.ts
@@ -31,6 +31,14 @@ export class FrameBuilder implements INodeType {
 				description: 'URL of frame image',
 			},
 			{
+				displayName: 'Open Frames Support',
+				name: 'openFrames',
+				type: 'boolean',
+				default: true,
+				description: 'Whether to include additional tags to conform to the Open Frames specification',
+				hint: 'Refer the <a href="https://github.com/open-frames/standard" target="_blank">Open Frames</a> standard for details.'
+			},
+			{
 				displayName: 'Buttons',
 				name: 'buttons',
 				type: 'fixedCollection',
@@ -203,6 +211,7 @@ export class FrameBuilder implements INodeType {
 		const returnData: INodeExecutionData[] = items.map((item, i) => {
 			const image = this.getNodeParameter('image', i) as string;
 			const buttons = this.getNodeParameter('buttons', i) as [any];
+			const openFrames = this.getNodeParameter('openFrames', i) as boolean;
 			const {
 				imageAspectRatio = '1.91:1',
 				inputTextLabel,
@@ -224,26 +233,57 @@ export class FrameBuilder implements INodeType {
 			addMetaTag('og:image', image);
 			addMetaTag('fc:frame:image', image);
 			addMetaTag('fc:frame:image:aspect_ratio', imageAspectRatio);
+			if (openFrames) {
+				/** @see {@link https://github.com/open-frames/standard?tab=readme-ov-file#required-properties} */
+				// The version label of the Open Frames spec
+				// Currently the only supported version is "vNext"
+				addMetaTag('of:version', 'vNext');
+				addMetaTag('of:accepts', 'vNext');
+				addMetaTag('of:image', image);
+				addMetaTag('of:image:aspect_ratio', imageAspectRatio);
+				addMetaTag('of:accepts:farcaster', version);
+				//TODO: Maybe also support "of:accepts:anonymous"?
+				//TODO: Maybe also support "of:image:alt"?
+			}
 			if (postUrl) {
 				addMetaTag('fc:frame:post_url', postUrl);
+				if (openFrames) {
+					addMetaTag('of:post_url', postUrl);
+				}
 			}
 			if ('items' in buttons) {
 				buttons.items.forEach((button, i) => {
 					addMetaTag(`fc:frame:button:${i + 1}`, button.label);
 					addMetaTag(`fc:frame:button:${i + 1}:action`, button.action);
+					if (openFrames) {
+						addMetaTag(`of:button:${i + 1}`, button.label);
+						addMetaTag(`of:button:${i + 1}:action`, button.action);
+					}
 					if (button.target) {
 						addMetaTag(`fc:frame:button:${i + 1}:target`, button.target);
+						if (openFrames) {
+							addMetaTag(`of:button:${i + 1}:target`, button.target);
+						}
 					}
 					if (button.postUrl) {
 						addMetaTag(`fc:frame:button:${i + 1}:post_url`, button.postUrl);
+						if (openFrames) {
+							addMetaTag(`of:button:${i + 1}:post_url`, button.postUrl);
+						}
 					}
 				});
 			}
 			if (inputTextLabel) {
 				addMetaTag('fc:frame:input:text', inputTextLabel);
+				if (openFrames) {
+					addMetaTag('of:input:text', inputTextLabel);
+				}
 			}
 			if (state && state !== '{}') {
 				addMetaTag('fc:frame:state', state);
+				if (openFrames) {
+					addMetaTag('of:state', state);
+				}
 			}
 			customMeta?.items.forEach(({ name, value }) => addMetaTag(name, value));
 			const html = dom.serialize();
